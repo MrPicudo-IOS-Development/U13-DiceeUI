ef1e3f1170c99000febb016b795a6611a00fc48f
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-26 13:37:43 -0600

    Proyecto creado
    Se creó la base para el repositorio de GitHub

---
+++ DiceeUI/ContentView.swift
@@ -0,0 +1,21 @@
+/* ContentView.swift --> DiceeUI. Created by Miguel Torres on 26/02/23. */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceeUI/DiceeUIApp.swift
@@ -0,0 +1,12 @@
+/* DiceeUIApp.swift --> DiceeUI. Created by Miguel Torres on 26/02/23. */
+
+import SwiftUI
+
+@main
+struct DiceeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
