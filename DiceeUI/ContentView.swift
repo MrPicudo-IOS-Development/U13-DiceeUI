1085d6e5a676343f44e75505e75f4900a37b2912
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-26 14:46:53 -0600

    Creación de recursos
    Se crearon todos los assets para la aplicación en Canva y quedaron mucho mejor que los que estaban disponibles en el curso.

--- DiceeUI/ContentView.swift
+++ DiceeUI/ContentView.swift
@@ -4,13 +4,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("DiceeBack")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("Las Vegas")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 400, height: 200)
+                Text("¡Presiona el botón para lanzar los dados!")
+                HStack {
+                    Image("Cara 1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100.0, height: 100.0)
+                    Image("Cara 2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100.0, height: 100.0)
+                }
+            }
         }
-        .padding()
     }
 }
 
