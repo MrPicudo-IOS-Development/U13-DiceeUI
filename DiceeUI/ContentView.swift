e96c7092877019c85c875605c95010506a4cf599
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-27 08:25:01 -0600

    Proyecto terminado
    Se modificaron los espacios entre los objetos de la interfaz, se agregó una funcionalidad básica al botón para cambiar la imagen mostrada usando los estados de la app en Swift UI.

--- DiceeUI/ContentView.swift
+++ DiceeUI/ContentView.swift
@@ -3,6 +3,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 3
+    @State var rightDiceNumber = 5
+    
     var body: some View {
         ZStack {
             Image("DiceeBack")
@@ -10,21 +14,28 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea(.all)
             VStack {
+                Spacer()
                 Image("Las Vegas")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 400, height: 200)
                 Text("¡Presiona el botón para lanzar los dados!")
+                Spacer()
                 HStack {
-                    Image("Cara 1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100.0, height: 100.0)
-                    Image("Cara 2")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100.0, height: 100.0)
+                    Dado(n: leftDiceNumber)
+                    Dado(n: rightDiceNumber)
                 }
+                Spacer()
+                Button("Lanzar dados") {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white) // Text color
+                    .buttonStyle(.borderedProminent) // Ajusta el padding del texto dentro del botón y da forma ovalada al contorno.
+                    .tint(.black) // Color of bordered button
+                Spacer()
             }
         }
     }
@@ -33,5 +44,15 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Dado: View {
+    let n: Int
+    var body: some View {
+        Image("Cara \(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all, 30.0)
     }
 }
